4541fd7d9db8facee09a17f9a59a6818cfff483a
Tapasweni Pathak <tapaswenipathak@gmail.com>
2016-07-25 08:38:06 +0530

    PEP8 files

--- FOSSBytes_Headlines.py
+++ FOSSBytes_Headlines.py
@@ -10,21 +10,22 @@ from bs4 import *
 from gi.repository import Notify
 from time import sleep
 
-def getFoss(title,message):
-	Notify.init("a")
-	notifyme=Notify.Notification.new(title,message,"Description:")
-	notifyme.show()
-	return
 
-url="http://fossbytes.com/"
+def getFoss(title, message):
+    Notify.init("a")
+    notifyme = Notify.Notification.new(title, message, "Description:")
+    notifyme.show()
+    return
+
+url = "http://fossbytes.com/"
 
 while True:
-	f=requests.get(url)
-	soup=BeautifulSoup(f.content,"lxml")
+    f = requests.get(url)
+    soup = BeautifulSoup(f.content, "lxml")
 
-	tech_head=soup.find_all("h2",{"itemprop":"headline"})
-	tech_desription=soup.find_all("h2",{"itemprop":"description"})
+    tech_head = soup.find_all("h2", {"itemprop": "headline"})
+    tech_desription = soup.find_all("h2", {"itemprop": "description"})
 
-	for info in tech_head:
-		getFoss("Tech news",info.text,)
-		sleep(60)
+    for info in tech_head:
+        getFoss("Tech news", info.text,)
+        sleep(60)
--- NDTV_Top_Stories.py
+++ NDTV_Top_Stories.py
@@ -10,21 +10,22 @@ import requests
 from time import sleep
 from gi.repository import Notify
 
-def getnotif(title, message):
-	Notify.init("Test")
-	notif=Notify.Notification.new(title,message, "Description:")
-	notif.show()
-	return
 
-url="http://www.ndtv.com/top-stories?pfrom=home-topstories"
+def getnotif(title, message):
+    Notify.init("Test")
+    notif = Notify.Notification.new(title, message, "Description:")
+    notif.show()
+    return
+
+url = "http://www.ndtv.com/top-stories?pfrom=home-topstories"
 
 while True:
 
-	r=requests.get(url)
-	soup=BeautifulSoup(r.content,"lxml")
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, "lxml")
 
-	data=soup.find_all("div",{"class":"nstory_header"})
+    data = soup.find_all("div", {"class": "nstory_header"})
 
-	for items in data:
-		getnotif("Top stories",items.text)
-		sleep(60)
+    for items in data:
+        getnotif("Top stories", items.text)
+        sleep(60)
